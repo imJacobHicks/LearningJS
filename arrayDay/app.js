1a8fc0d62a341243283919ffb089b73ada6c14b4
Jacob Hicks <85461494+imJacobHicks@users.noreply.github.com>
2022-02-08 21:25:59 -0500

    Add files via upload

---
+++ arrayDay/app.js
@@ -0,0 +1,113 @@
+// Get your shorts on - this is an array workout!
+// ## Array Cardio Day 1
+
+// Some data we can work with
+const inventors = [
+    { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+    { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+    { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+    { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+    { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+    { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+    { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+    { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
+    { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+    { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+    { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+    { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
+];
+  
+const people = [
+    'Bernhard, Sandra', 'Bethea, Erin', 'Becker, Carl', 'Bentsen, Lloyd', 'Beckett, Samuel', 'Blake, William', 'Berger, Ric', 'Beddoes, Mick', 'Beethoven, Ludwig',
+    'Belloc, Hilaire', 'Begin, Menachem', 'Bellow, Saul', 'Benchley, Robert', 'Blair, Robert', 'Benenson, Peter', 'Benjamin, Walter', 'Berlin, Irving',
+    'Benn, Tony', 'Benson, Leana', 'Bent, Silas', 'Berle, Milton', 'Berry, Halle', 'Biko, Steve', 'Beck, Glenn', 'Bergman, Ingmar', 'Black, Elk', 'Berio, Luciano',
+    'Berne, Eric', 'Berra, Yogi', 'Berry, Wendell', 'Bevan, Aneurin', 'Ben-Gurion, David', 'Bevel, Ken', 'Biden, Joseph', 'Bennington, Chester', 'Bierce, Ambrose',
+    'Billings, Josh', 'Birrell, Augustine', 'Blair, Tony', 'Beecher, Henry', 'Biondo, Frank'
+];
+      
+// Array.prototype.filter()
+// 1. Filter the list of inventors for those who were born in the 1500's
+const fifteen = inventors.filter(inventor => { 
+    if(inventor.year >= 1500 && inventor.year < 1600){ //if inventor born between 1500 and 1599
+        return true;// keep it!
+    }
+});
+
+console.log(fifteen);
+
+// Array.prototype.map()
+// 2. Give us an array of the inventors first and last names
+const  fullNames = inventors.map(inventor => `${inventor.first} ${inventor.last}`);
+console.log(fullNames);
+
+
+// Array.prototype.sort()
+// 3. Sort the inventors by birth date, oldest to youngest 
+const ordered = inventors.sort(function(a, b) {
+    if (a.year > b.year) {
+        return 1;
+    } else {
+        return -1;
+    }
+});
+
+console.table(ordered);
+
+// Array.prototype.reduce()
+// 4. How many years did all the inventors live all together?
+const totalYears = inventors.reduce((total, inventor) => {
+    return total + (inventor.passed - inventor.year);
+}, 0); // This 0 makes the total start at 0 to add up the total
+
+console.log(totalYears);
+
+// 5. Sort the inventors by years lived
+const oldest = inventors.sort(function(a,b) {
+    const lastGuy = a.passed - a.year;
+    const nextGuy = b.passed - b.year;
+    if(lastGuy > nextGuy) {
+        return -1;
+    } else {
+        return 1;
+    }
+});
+
+console.table(oldest);
+ 
+// 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
+// https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+// open link and run below code in console. It will prompt "undefined" and you need to specify de
+// const category = document.querySelector('.mw-category');
+// const links = Array.from(category.querySelectorAll('a'));
+// const de = links
+//                 .map(link => link.textContent)   //stack multiple on top of "links"
+//                 .filter(streetName => streetName.includes('de'));    //filters for boulevards with "de"
+
+// 7. sort Exercise
+// Sort the people alphabetically by last name
+const alpha = people.sort((lastOne, nextOne) => {
+    const [aLast, aFirst] = lastOne.split(', '); // removing space and comma from name so they are their own separate variables
+    const [bLast, bFirst] = nextOne.split(', ');
+    //ordering them alphabetically by last name
+    if(aLast > bLast) {
+        return 1;
+    } else {
+        return -1;
+    }
+});
+
+console.log(alpha);
+
+// 8. Reduce Exercise
+// Sum up the instances of each of these
+const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
+
+const transportation = data.reduce(function(object, item) {
+    if(!object[item]) {
+        object[item] = 0; //starting at 0
+    }
+    object[item]++; //adding until no more to loop over
+    return object;
+}, {} ); //starting with blank object {}
+
+console.log(transportation);
